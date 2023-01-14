521e6e29e638f5aa09008a81df0dd0aac6de8cf0
Antoni Seba <antoniwoj@o2.pl>
2023-01-14 22:21:49 +0100

    Add discord webhook functionality

--- src/main/java/me/metallicgoat/prizecommands/events/LoseWinPrizes.java
+++ src/main/java/me/metallicgoat/prizecommands/events/LoseWinPrizes.java
@@ -12,10 +12,12 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
+import me.metallicgoat.prizecommands.events.DiscordWebhook;
 
 public class LoseWinPrizes implements Listener {
 
@@ -61,8 +63,48 @@ public class LoseWinPrizes implements Listener {
                 placeholderReplacements.put("winner-team-color-code", "&" + e.getWinnerTeam().getChatColor().getChar());
             }
 
+            String winner = e.getWinnerTeam().name();
+
+            DiscordWebhook webhook = new DiscordWebhook("\n" +
+                    "https://discord.com/api/webhooks/1063774393412632596/HZxLsvDPk-29W9d8IqR_lo5y75hVjYhbaFioLQbImMqtVI6N8_YMdhJ5tSGTCj_N0Q96");
+            webhook.setContent(winner + " wygrał grę!");
+            webhook.setAvatarUrl("https://your.awesome/image.png");
+            webhook.setUsername("Speedpvp.eu");
+            webhook.setTts(true);
+            webhook.addEmbed(new DiscordWebhook.EmbedObject()
+                    .setTitle("Gra bedwars - wynik")
+                    .setDescription(winner + " wygrał grę!")
+                    .setColor(Color.RED)
+                    .addField("Wynik", winner + " wygrał grę!", false)
+                    .setFooter("Speedpvp.eu", "https://i.imgur.com/3vZ7sEw.png")
+                    .setThumbnail("https://i.imgur.com/3vZ7sEw.png")
+                    .setUrl("https://i.imgur.com/3vZ7sEw.png")
+                    .setAuthor("Speedpvp.eu", "https://i.imgur.com/3vZ7sEw.png", "https://i.imgur.com/3vZ7sEw.png"));
+//                    .setDescription("Wygrał zespół " + winner)
+//                    .setColor(Color.RED));
+//                    .addField("1st Field", loser, true)
+//                    .addField("2nd Field", winner, true)
+//                    .addField("3rd Field", "No-Inline", false)
+//                    .setThumbnail("https://kryptongta.com/images/kryptonlogo.png")
+//                    .setFooter("Footer text", "https://kryptongta.com/images/kryptonlogodark.png")
+//                    .setImage("https://kryptongta.com/images/kryptontitle2.png")
+//                    .setAuthor("Author Name", "https://kryptongta.com", "https://kryptongta.com/images/kryptonlogowide.png")
+//                    .setUrl("https://kryptongta.com"));
+            webhook.addEmbed(new DiscordWebhook.EmbedObject()
+                    .setDescription("Just another added embed object!"));
+
+            try {
+                webhook.execute(); //Handle exception
+            }
+            catch (Exception ex) {
+                ex.printStackTrace();
+            }
+
             for(Player player : activePlayers) {
                 final List<Prize> endPrize = e.getWinners().contains(player) ? ConfigValue.playerWinPrize : ConfigValue.playerLosePrize;
+
+                // use the DiscordWebhook class to send a message to the discord channel
+
 
                 for (Prize prize: endPrize)
                     prize.earn(arena, player, placeholderReplacements);
